8bd81ae318dd818603f52d4c7dd63159777da93e
BeitxuelaWEB <bearobledillogomez@gmail.com>
2022-06-29 11:15:02 +0200

    add components

---
+++ src/components/WeatherPanel.js
@@ -0,0 +1,82 @@
+import React, {useState} from 'react';
+import Form from './Form';
+import Card from './Card';
+
+const WeatherPanel = () => {
+
+    let urlWeather="https://api.openweathermap.org/data/2.5/weather?appid=a8b18bf54ff9eb2e6d218ae18ae7bf09&lang=es";
+    let cityUrl="&q=";
+
+    let urlForecast= "https://api.openweathermap.org/data/2.5/forecast?appid=a8b18bf54ff9eb2e6d218ae18ae7bf09&lang=es";
+    const [weather, setWeather] = useState([]);
+    const [forecast, setForecast] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [show, setShow] = useState(false);
+    const [location, setLocation] = useState("");
+
+    const getLocation = async(loc) => {
+        setLoading(true);
+        setLocation(loc);
+
+        //weather
+
+        urlWeather = urlWeather + cityUrl + loc;
+
+        await fetch(urlWeather).then((response) =>{
+            if(!response.ok) throw {response}
+            return response.json();
+        }).then((weatherData) =>{
+            console.log(weatherData);
+            setWeather(weatherData);
+        }).catch(error =>{
+            console.log(error);
+            setLoading(false);
+            setShow(false);
+        });
+
+        //Forecast
+
+        urlForecast = urlForecast + cityUrl + loc;
+
+        await fetch(urlForecast).then((response) =>{
+            if(!response.ok) throw {response}
+            return response.json();
+        }).then((forecastData) =>{
+            console.log(forecastData);
+            setForecast(forecastData);
+
+            setLoading(false);
+            setShow(true);
+
+        }).catch(error =>{
+            console.log(error);
+            setLoading(false);
+            setShow(false);
+        });
+
+    }
+
+
+    return(
+
+        <React.Fragment>
+    
+            <Form
+                newLocation = {getLocation}
+            />
+
+            <Card
+                showData = {show}
+                loadingData = {loading}
+                weather = {weather}
+                forecast = {forecast}
+            />
+
+
+        </React.Fragment>
+
+    );
+
+}
+
+export default WeatherPanel;
